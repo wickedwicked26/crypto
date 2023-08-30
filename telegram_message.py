364311d6ff4504bb5bc79a52846d4c536fafa21a
miguel <lermontoff2611@gmail.com>
2023-08-30 15:46:41 +0300

    get chat id function was removed to telegram project

--- telegram_message.py
+++ telegram_message.py
@@ -8,20 +8,6 @@ chat_ids = ['5442883627', '1934151160']
 path = '/home/miguel/PycharmProjects/crypto/'
 
 
-# @bot.message_handler(commands=['getchatid'])
-# def get_chat_id(message):
-#     chat_id = message.chat.id
-#     bot.reply_to(message, f'Ваш чат айди: {chat_id}')
-#
-#
-# def main():
-#     bot.polling(none_stop=True)
-#
-#
-# if __name__ == '__main__':
-#     main()
-
-
 def send_graph(symbol, timestamp, message):
     for i in chat_ids:
         user_id = i
